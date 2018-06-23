ac736c8e1dd3adbffd3775c5a76e8890596f3f97
gabriellgpc <gabriellgpc@hotmail.com>
2018-06-23 00:55:24 -0300

    insere e insereEcorrige. Resultados não é uma AA, problema possivelmente nas rotações.

--- PraticaMOD7/Aa.java
+++ PraticaMOD7/Aa.java
@@ -40,17 +40,37 @@ public class Aa{
   //TODO rotacao para a direta apartir da Raiz a
   static void rodeDir(Aa a){
     if(a == null)throw new Error("Rotacao invalida, arvore Vazia\n");
+    if(a.esq == null)return;
+
     if(a.esq.cor == N)return;
-    Aa tmp = new Aa(a.value, a.cor, a.esq.dir, a.dir);
-    a.copy(new Aa(a.esq.value, a.esq.cor, a.esq.esq, tmp));
+    Aa right = new Aa(a.value, a.cor, a.esq.dir, a.dir);
+    a.copy(new Aa(a.esq.value, a.esq.cor, a.esq.esq, right));
   }
   static void rodeEsq(Aa a){
     if(a == null)throw new Error("Rotacao invalida, arvore Vazia\n");
+    if(a.dir == null || a.dir.dir == null)return;
 
     if(a.dir.cor != R && a.dir.dir.cor != R)return;
     Aa left = new Aa(a.value, a.cor, a.esq, a.dir.esq);
     a.copy(new Aa(a.dir.value, a.dir.cor, left ,a.dir.dir));
     a.dir.cor = N;
+  }
+
+  static void insereECorrigeRaiz(Aa a,int i){
+    if(a == null)return;
+    a = insere(a,i);
+    a.cor = N;
+  }
+
+  static Aa insere(Aa a,int i){
+    if(a == null)return new Aa(i,R,null,null);
+    if(i > a.value)
+      a.dir = insere(a.dir,i);
+    else
+      a.esq = insere(a.esq,i);
+    rodeDir(a);
+    rodeEsq(a);
+    return a;
   }
 
   static void teste1(){
@@ -94,9 +114,20 @@ public class Aa{
     new Fenetre(a,new String("Pos rotação"));
   }
 
+  static void teste4(){
+    Aa a = new Aa(9,R,null,null);
+    insere(a,6);
+    insere(a,3);
+    insere(a,1);
+    insere(a,4);
+    insere(a,5);
+    insereECorrigeRaiz(a,8);
+    new Fenetre(a,"resultado");
+  }
   public static void main (String [] args) {
     // teste1();
     // teste2();
-    teste3();
+    // teste3();
+    teste4();
   }
 }
